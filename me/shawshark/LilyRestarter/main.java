a5bf3012eadc4d25cfc6fac670a73ab5e3ba1ee9
shawshark <chrissbolding@gmail.com>
2013-11-06 22:08:11 +1100

    Updated from 1.3 to 1.4

--- me/shawshark/LilyRestarter/main.java
+++ me/shawshark/LilyRestarter/main.java
@@ -1,5 +1,9 @@
 package me.shawshark.LilyRestarter;
 
+import lilypad.client.connect.api.Connect;
+import lilypad.client.connect.api.request.RequestException;
+import lilypad.client.connect.api.request.impl.RedirectRequest;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -10,95 +14,116 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class main extends JavaPlugin implements Listener {
 
+	Connect connect;
 	
-	public void onEnable()
-	  {
-		saveDefaultConfig(); 
-	      getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
-			@Override
-			public void run() {
-				getServer().dispatchCommand(getServer().getConsoleSender(), 
-						"alert " + (getConfig().getString("alertmessage")));	
-			}	
-	    }, 200L);
-	  }
-	
-	public void onDisable() {
-		  
-		  saveDefaultConfig(); 
-		  sendplayers();
-		  
-		  getServer().dispatchCommand(getServer().getConsoleSender(), 
-				  "alert " + (getConfig().getString("shutdownalertmessage")));
-		}
-	  
-	  public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) { 
-	   {
+        public void onEnable()
+          {   
+        	 this.connect = ((Connect)getServer().getServicesManager().getRegistration(Connect.class).getProvider());
+        	  
+        	 saveDefaultConfig(); 
+             getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+               @Override
+               public void run() {
+                    getServer().dispatchCommand(getServer().getConsoleSender(), 
+                               "alert " + (getConfig().getString("alertmessage")));        
+                    }        
+               }, 200L);
+          }
+        
+        public void onDisable() 
+        {
+        	redirectevent();      
+            saveDefaultConfig(); 
+            getServer().dispatchCommand(getServer().getConsoleSender(), 
+                                  "alert " + (getConfig().getString("shutdownalertmessage")));
+        }
+          
+        public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) { 
+        
+         {
         Player p = (Player)sender;
+        
         if (p.hasPermission("lilyrestarter.restart") || (p.hasPermission("lilyrestarter.admin"))) {
             if (cmd.getName().equalsIgnoreCase("restartserver")) {
-            	
-            	Bukkit.broadcastMessage(ChatColor.RED + (getConfig().getString("beforerestartmessage")));
-            	
-            	getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
-	            	public void run() {
-	            		
-	            		Bukkit.broadcastMessage(ChatColor.RED + (getConfig().getString("broadcastmessage")));
-	            		
-	            		shutdowndebug();	
-	            	}
-	            }
-	            , 60L);         
+                    
+                    Bukkit.broadcastMessage(ChatColor.RED + (getConfig().getString("beforerestartmessage")));
+                    
+                    getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+                            public void run() {
+                                    Bukkit.broadcastMessage(ChatColor.RED + (getConfig().getString("broadcastmessage")));
+                                    shutdown();
+                            }
+                    }
+                    , 60L);         
             }
             
-            if (p.hasPermission("lilyrestarter.sendallplayers") || (p.hasPermission("lilyrestarter.admin"))) {
-              if (cmd.getName().equalsIgnoreCase("sendallplayers")) {
-                	
-                sendplayers();
-                
-                getServer().dispatchCommand(getServer().getConsoleSender(), 
-                   "send " + sender.getName() + " "+ (getConfig().getString("servername")));
-                
-          		getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable(){
-          			
-          	@Override
-            public void run() {
-
-            	Bukkit.broadcastMessage(ChatColor.GRAY + "[" + ChatColor.RED + "LilyRestarter" + ChatColor.GRAY + "]"
-            			
-                    + ChatColor.GRAY + " All players were successfully sent to the target server!");
-                			
-          					}	
-          				}, 20L);
-              		}
-            	} 
-        	}	     
-	   	}
-	   return true;     
-	  }
-    	
-      public void sendplayers() {
-	   for (Player p : getServer().getOnlinePlayers())
-        	p.performCommand(getConfig().getString("playercommand"));
+            if (p.hasPermission("lilyrestarter.redirectall") || (p.hasPermission("lilyrestarter.admin"))) {
+              if (cmd.getName().equalsIgnoreCase("redirectall") || (cmd.getName().equalsIgnoreCase("sendallplayers"))) {
+                        
+            	  
+            	  Bukkit.getServer().broadcastMessage(ChatColor.RED + "Teleporting all players to the " + 
+            			  							getConfig().getString("redirectserver" + ChatColor.RED + " server!"));
+            	  
+            	  getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+                      public void run() {
+                      redirectevent();     
+                      }
+              }
+              , 60L);         
+            	  
+             }
+           } 
+         }             
+        }
+       return true;     
+    }
+        
+        /* @EventHandler(priority=EventPriority.HIGHEST)
+        public void kickevent(final PlayerKickEvent e){
+            Player player = e.getPlayer();
+            {
+            	e.setCancelled(true);
+            	player.sendMessage(ChatColor.RED + "You have been kicked from the server for");
+                player.sendMessage(ChatColor.GREEN + e.getReason());            }
+            redirectplayer();
+            
+        } */
+        
+        public void redirectevent() {
+    	  for (Player p : getServer().getOnlinePlayers())
+    	  redirect(getConfig().getString("redirectserver"), p); // Only use to send all players
       }
+        
+        /* public void redirectplayer() {
+      	  redirect(getConfig().getString("redirectserver"), p); // Only use to send 1 player
+        } */
+            
+      public void sendplayers() {
+           for (Player p : getServer().getOnlinePlayers())
+                p.performCommand(getConfig().getString("playercommand"));
+      } // Removing this one step at a time.
       
       public void shutdown() {
-    	  Bukkit.getServer().shutdown(); 
+              Bukkit.getServer().shutdown(); 
        }
       
       public void shutdowndebug() 
-      
       { getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
 
-			@Override
-			public void run() 
-			{ shutdown(); }
-    	  			}, 20L);
-    	  }
+           @Override
+           public void run() 
+           { shutdown(); }}, 20L);
+      }
+      
+      
+      public void redirect(String string, Player player) {
+          try { connect.request(new RedirectRequest(getConfig().getString("redirectServer"), player.getName()));
+          } catch (RequestException e) { e.printStackTrace(); }
+      }
       
       public void debug() { /* Not being used at the moment */
-    	  Bukkit.broadcastMessage(ChatColor.RED + "DEBUG MESSAGE!");
-    	  Bukkit.broadcastMessage(ChatColor.RED + "DEBUG MESSAGE!");
-    	  Bukkit.broadcastMessage(ChatColor.RED + "DEBUG MESSAGE!");
+              Bukkit.broadcastMessage(ChatColor.RED + "DEBUG MESSAGE!");
+              Bukkit.broadcastMessage(ChatColor.RED + "DEBUG MESSAGE!");
+              Bukkit.broadcastMessage(ChatColor.RED + "DEBUG MESSAGE!");
       }
 }
